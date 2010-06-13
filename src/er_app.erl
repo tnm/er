d1aa69d81259ac187eba8abcae711e51dd2b4e61
Matt Stancliff <matt@genges.com>
2010-06-13 16:00:11 -0500

    Add start/0 to er_app since we don't need arguments
    That's right -- we don't need no stinkin' arguments.

--- src/er_app.erl
+++ src/er_app.erl
@@ -3,14 +3,17 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
-start(_StartType, _StartArgs) ->
+start() -> 
   er_sup:start_link().
+
+start(_StartType, _StartArgs) ->
+  start().
 
 stop(_State) ->
   ok.
